d9a094932e0dd74441603a50cae2fa617bb349aa
vamsi560 <145781997+vamsi560@users.noreply.github.com>
2026-02-06 15:29:55 +0000

    cors again

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import api
 
+
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://happy-flower-02c7dc30f.4.azurestaticapps.net"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api.router)
 
 @app.get("/")
