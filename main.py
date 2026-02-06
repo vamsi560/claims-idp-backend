fbd399f3e38f7946d46a815a8c3266b1a1842109
vamsi560 <145781997+vamsi560@users.noreply.github.com>
2026-02-06 15:17:24 +0000

    cors fix

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://happy-flower-02c7dc30f.4.azurestaticapps.net"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
